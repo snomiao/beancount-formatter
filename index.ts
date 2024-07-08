bc0bbb66d89ce17c75d27df71c00b4e0c4b458c8
snomiao <snomiao@gmail.com>
2024-07-08 14:50:21 +0000

    chore(tsconfig): no dom needed
    no dom needed

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-export { format } from "./vscode-beancount-formatter/src/format.ts";
+import { format as f } from "./vscode-beancount-formatter/src/format.ts";
+
+export interface FormatOptions {
+  prefixWidth?: number;
+  numWidth?: number;
+  currencyColumn?: number;
+  fixedCJKWidth?: boolean;
+}
+
+/**
+ * Format contents.
+ *
+ * @param contents Contents waiting to be formatted.
+ * @param config format argument
+ */
+export function format(contents: string, opts?: FormatOptions): string {
+  return f(contents, opts);
+}
